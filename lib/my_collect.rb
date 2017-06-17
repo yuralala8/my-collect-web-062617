f95b94930952d017d48acd0deae2112ac33ceb9f
yuralala8 <yc1435@nyu.edu>
2017-06-17 16:58:12 -0400

    my collect commit

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+    my_collection = []
+    i = 0
+    while i < collection.length
+        my_collection << yield(collection[i])
+        i = i + 1
+    end
+    my_collection
+end
